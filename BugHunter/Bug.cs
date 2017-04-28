8f6198907018bcbc28a01fbf27f87c00fc62a7d0
walt speelman <2963975+waltiam@users.noreply.github.com>
2017-04-28 16:50:23 -0400

    initial commit

---
+++ BugHunter/Bug.cs
@@ -0,0 +1,34 @@
+﻿namespace BugHunter
+{
+    public class Bug
+    {
+
+        private bool dodging;
+        private bool dead;
+
+        public void Dodge()
+        {
+            dodging = true;
+        }
+
+        public void Hit()
+        {
+            dead = true;
+        }
+
+        public void Miss()
+        {
+            dodging = false;
+        }
+
+        public bool IsDodging()
+        {
+            return dodging;
+        }
+
+        public bool IsDead()
+        {
+            return dead;
+        }
+    }
+}
