182ac000fc6c8243e4973c57ab0d65654a9ce80c
walt <2963975+waltiam@users.noreply.github.com>
2017-04-30 22:53:43 -0400

    added spec created feature file

---
+++ BugHunter.xUnit/BugBlasterSteps.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System;
+using TechTalk.SpecFlow;
+
+namespace BugHunter.xUnit
+{
+    [Binding]
+    public class BugBlasterSteps
+    {
+        private Raygun _raygun;
+        private Bug _bug;
+
+        [Given(@"I have a raygun")]
+        public void GivenIHaveARaygun()
+        {
+            _raygun = new Raygun();
+        }
+        
+        [Given(@"I have at least (.*) charge")]
+        public void GivenIHaveAtLeastCharge(int p0)
+        {
+            _raygun.Recharge();
+        }
+        
+        [Given(@"I have a stationary target bug")]
+        public void GivenIHaveAStationaryTargetBug()
+        {
+            _bug = new Bug();
+        }
+        
+        [When(@"I fire at the bug")]
+        public void WhenIFireAtTheBug()
+        {
+            _raygun.FireAt(_bug);
+        }
+        
+        [Then(@"the result should be a dead bug")]
+        public void ThenTheResultShouldBeADeadBug()
+        {
+            _bug.IsDead.Should().BeTrue();
+        }
+    }
+}
---
+++ BugHunter.xUnit/spec/BugBlaster.feature
@@ -0,0 +1,12 @@
+﻿Feature: BugBlaster
+	In order to avoid being overrun by bus
+	As a space marine
+	I want to be able to blast bugs
+
+@mytag
+Scenario: Blast a bug
+	Given I have a raygun
+	And I have at least 1 charge
+	And I have a stationary target bug
+	When I fire at the bug
+	Then the result should be a dead bug
