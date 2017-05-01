11dd3ac0985f4c69913e25568f0a3a5844ed45e0
walt <2963975+waltiam@users.noreply.github.com>
2017-04-30 20:16:53 -0400

    added the unit test added xunit adde fluent ssertions

---
+++ BugHunter.xUnit/BugHunterFluentAssertions.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace BugHunter.xUnit
+{
+
+    public class BugHunterFluentAssertions
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void FireMultipleTimes(int fireCount)
+        {
+
+            Bug bug = new Bug();
+            Raygun gun = new Raygun();
+
+            for (int i = 0; i < fireCount; i++)
+            {
+                gun.FireAt(bug);
+            }
+
+            if (fireCount >= 3)
+            {
+                gun.HasAmmo().Should().BeFalse();
+            }
+            else
+            {
+                gun.HasAmmo().Should().BeTrue();
+            }
+        }
+    }
+}
+
---
+++ BugHunter.xUnit/BugHunterXunit.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace BugHunter.xUnit
+{
+    public class BugHunterXunit
+    {
+        [Theory]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void TestBugCanDodge(bool didDodge, bool shouldBeDead)
+        {
+            var bug = new Bug();
+            var rayGun = new Raygun();
+
+            if (didDodge)
+            {
+                bug.Dodge();
+            }
+
+            rayGun.FireAt(bug);
+
+            if (shouldBeDead)
+            {
+                Assert.True(bug.IsDead());
+            }
+            else { Assert.False(bug.IsDead()); }
+        }
+    }
+}
+
