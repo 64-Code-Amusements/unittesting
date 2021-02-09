c5985e14774f17262e3f58cf1e9505fd42a36aed
walt <2963975+waltiam@users.noreply.github.com>
2021-02-09 17:17:23 +0000

    rev to net core 5.0

--- BugHunter.xUnit/BugHunter_nSubstitue.cs
+++ BugHunter.xUnit/BugHunter_nSubstitue.cs
@@ -7,6 +7,17 @@ namespace BugHunter.xUnit
     public class BugHunter_nSubstitue
     {
         [Fact]
+        public void BugsGetAChanceToDodge(){
+            var bug = Substitute.For<IBug>();
+            bug.IsDodging.Returns(false);
+            var gun = new Raygun();
+
+            gun.FireAt(bug);
+
+            bug.Received().IsDodging = true;
+        }
+
+        [Fact]
         public void TryShootingBug()
         {
             var bug = Substitute.For<IBug>();
@@ -18,6 +29,7 @@ namespace BugHunter.xUnit
             bug.Received().Hit();
             bug.DidNotReceive().Miss();
         }
+
         [Fact]
         public void TryShootingADodgingBug()
         {
